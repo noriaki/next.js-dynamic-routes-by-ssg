7e6b13a760e4d98fe2b72a5682fab3f57fd108b2
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-04-02 20:41:07 +0900

    add random color pages

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { customAlphabet } from 'nanoid/non-secure';
 
-const AnySlugsPage: NextPage = () => {
+import styles from '../styles/ColorsPage.module.css';
+
+const nanoid = customAlphabet('1234567890abcdef', 6);
+
+const ColorsPage: NextPage = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>Welcome to Next.js! params: {slug}</div>
+  const color = nanoid();
+  return (
+    <div>
+      <p>Welcome to Random color page!</p>
+      <p>
+        Current color: #{slug}
+        <span
+          className={styles.box}
+          style={{ backgroundColor: `#${slug}` }}
+        />
+      </p>
+      <Link href="/[slug]" as={`/${color}`}>
+        <a>
+          Link to random color #{color}
+          <span
+            className={styles.box}
+            style={{ backgroundColor: `#${color}` }}
+          />
+        </a>
+      </Link>
+    </div>
+  );
 }
 
-export default AnySlugsPage;
+export default ColorsPage;
